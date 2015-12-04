b9e8495159494213100802b554cd760743a05c31
André Santos <andrerfcsantos@gmail.com>
2015-12-04 18:39:25 +0000

    Add solution problem 01

---
+++ src/problems/Day_01.java
@@ -0,0 +1,51 @@
+package problems;
+
+import utils.InputHandler;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+/**
+ * Problem 01
+ */
+public class Day_01 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Solution Day 1 Problem 1: " + problem_01());
+        System.out.println("Solution Day 1 Problem 2: " + problem_02());
+    }
+
+    public static int problem_01() throws IOException {
+        int floor = 0;
+        ArrayList<String> lines = (ArrayList<String>) InputHandler.getLines("Inputfiles/day01_1.txt");
+        String line = lines.get(0);
+
+        for (int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == '(') floor++;
+            if (line.charAt(i) == ')') floor--;
+        }
+
+        return floor;
+    }
+
+    public static int problem_02() throws IOException {
+        int floor = 0;
+        ArrayList<String> lines = (ArrayList<String>) InputHandler.getLines("Inputfiles/day01_1.txt");
+        String line = lines.get(0);
+
+        for (int i = 0; i < line.length(); i++) {
+
+            if (line.charAt(i) == '(') floor++;
+            if (line.charAt(i) == ')') floor--;
+
+            if (floor == -1) {
+                floor = i + 1;
+                break;
+            }
+
+        }
+
+        return floor;
+    }
+
+}
---
+++ src/utils/InputHandler.java
@@ -0,0 +1,42 @@
+package utils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class that hosts the code for reading the input files.
+ */
+
+public final class InputHandler {
+    private InputHandler() {
+    }
+
+    public static List<String> getLines(String filePath) throws IOException {
+        ArrayList<String> lines = new ArrayList<>();
+        String line;
+        File filein = new File(filePath);
+        BufferedReader bin = new BufferedReader(new FileReader(filein));
+
+        while (bin.ready()) {
+            line = bin.readLine();
+            lines.add(line);
+        }
+
+        bin.close();
+
+        return lines;
+    }
+
+    public static void printLines(String filePath) throws IOException {
+        ArrayList<String> lines = (ArrayList<String>) getLines(filePath);
+
+        for (int i = 0; i < lines.size(); i++) {
+            System.out.println("Line " + i + ": " + lines.get(i));
+        }
+
+    }
+}
