9b9da01180df946bcdc4c874ce29b9b412b5df1c
André Santos <andrerfcsantos@gmail.com>
2015-12-10 23:04:51 +0000

    Add solution problem 10

---
+++ src/problems/Day_10.java
@@ -0,0 +1,80 @@
+package problems;
+
+import java.io.IOException;
+
+/**
+ * Created by Andre on 10-12-2015.
+ */
+public class Day_10 {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Solution Day 10 Problem 1: " + problem_01());
+        System.out.println("Solution Day 10 Problem 2: " + problem_02());
+    }
+
+    public static int problem_01() throws IOException {
+        int totalL=0, startStrL=0;
+        int qtdChar=0;
+        char currentChar='z', lastChar='z';
+        StringBuilder startingString = new StringBuilder("1321131112");
+        StringBuilder newstr;
+
+        for(int times=0;times<40;times++) {
+            lastChar=startingString.charAt(0);
+            newstr = new StringBuilder();
+            startStrL= startingString.length();
+
+            for (int i = 0; i < startStrL; i++) {
+
+                currentChar = startingString.charAt(i);
+                if (currentChar == lastChar) {
+                    qtdChar++;
+                }
+                else {
+                    newstr.append(qtdChar).append(lastChar);
+                    qtdChar = 1;
+                }
+                lastChar = currentChar;
+            }
+            newstr.append(qtdChar).append(lastChar);
+            qtdChar = 0;
+
+            startingString = new StringBuilder((newstr.toString()));
+        }
+
+        return startingString.length();
+    }
+
+    public static int problem_02() throws IOException {
+        int totalL=0, startStrL=0;
+        int qtdChar=0;
+        char currentChar='z', lastChar='z';
+        StringBuilder startingString = new StringBuilder("1321131112");
+        StringBuilder newstr;
+
+        for(int times=0;times<50;times++) {
+            lastChar=startingString.charAt(0);
+            newstr = new StringBuilder();
+            startStrL= startingString.length();
+
+            for (int i = 0; i < startStrL; i++) {
+
+                currentChar = startingString.charAt(i);
+                if (currentChar == lastChar) {
+                    qtdChar++;
+                }
+                else {
+                    newstr.append(qtdChar).append(lastChar);
+                    qtdChar = 1;
+                }
+                lastChar = currentChar;
+            }
+            newstr.append(qtdChar).append(lastChar);
+            qtdChar = 0;
+
+            startingString = new StringBuilder((newstr.toString()));
+        }
+
+        return startingString.length();
+    }
+}
