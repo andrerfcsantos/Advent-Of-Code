b4060c82279e49634c1974f64acb05737c1694ae
André Santos <andrerfcsantos@gmail.com>
2017-12-01 09:30:16 +0000

    [Day 01] Python solution

--- 2017/Python/day01.py
+++ 2017/Python/day01.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 
-import itertools as it
-import random as rd
-import fileinput  as fi
-import numpy as np
-import re
-import collections as col
-
 DAY = 1
+numbers = open(f'../in/day{DAY:02}.txt').readline().strip()
 
-part1, part2 = None, None
+sum, sum2 = 0,0
+step = int(len(numbers)/2)
 
-inp = open(f'../in/{DAY:02}.txt')
+for i,number in enumerate(numbers):
+    if number == numbers[(i+1)%len(numbers)]:
+        sum += int(number)
+    if number == numbers[(i+step)%len(numbers)]:
+        sum2 += int(number)
 
-for line in inp:
-    line = line.strip()
 
+part1, part2 = sum, sum2
 
 print(f'Part 1: {part1}')
 print(f'Part 2: {part2}')
 
-inp.close()
