d42847fc70efd9ddd75c8c238a75ad153e36c85e
André Santos <andrerfcsantos@gmail.com>
2017-12-01 22:14:01 +0000

    [Day 01] Simplifies a bit perl and python solutions
    On both solutions:
    - Removed some unnecessary variables and 
    declarations.
    - Added a variable holding the size of the input.

--- 2017/Perl/day01.pl
+++ 2017/Perl/day01.pl
@@ -5,24 +5,18 @@ chomp($input = <>);
 
 @input  = map(int,(split '', $input));
 @input_enum = map( [$_, $input[$_]], 0 .. $#input);
-$step = (scalar @input) / 2;
-
-$sum, $sum2;
+$size = scalar @input;
+$step = $size / 2;
 
 for (@input_enum) {
     ($i,$number) = @$_;
-    if ($number == $input[($i+1) % (scalar @input)]){
+    if ($number == $input[($i+1) % $size]){
         $sum += $number;
     }
-    if ($number == $input[($i+$step) % (scalar @input)]){
+    if ($number == $input[($i+$step) % $size]){
         $sum2 += $number;
     }
-
 }
 
 print "Part 1: $sum\n";
 print "Part 2: $sum2\n";
-
-
-
-
--- 2017/Python/day01.py
+++ 2017/Python/day01.py
@@ -4,17 +4,15 @@ DAY = 1
 numbers = open(f'../in/day{DAY:02}.txt').readline().strip()
 
 sum, sum2 = 0,0
-step = int(len(numbers)/2)
+size = len(numbers)
+step = size//2
 
 for i,number in enumerate(numbers):
-    if number == numbers[(i+1)%len(numbers)]:
+    if number == numbers[(i+1)%size]:
         sum += int(number)
-    if number == numbers[(i+step)%len(numbers)]:
+    if number == numbers[(i+step)%size]:
         sum2 += int(number)
 
-
-part1, part2 = sum, sum2
-
-print(f'Part 1: {part1}')
-print(f'Part 2: {part2}')
+print(f'Part 1: {sum}')
+print(f'Part 2: {sum2}')
 
