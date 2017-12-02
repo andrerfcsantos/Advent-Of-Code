d066db106aa6814313ae9e9fe352bda7200ffc27
André Santos <andrerfcsantos@gmail.com>
2017-12-02 19:08:36 +0000

    [Day 02] Python solution (for real this time)

---
+++ 2017/Python/day02.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+import itertools as it
+
+DAY = 2
+inp = open(f'../in/day{DAY:02}.txt')
+
+sum_p1, sum_p2  = 0,0
+
+for line in inp:
+    numbers = list(map(int,line.strip().split()))
+    minval, maxval = min(numbers),max(numbers)
+    sum_p1 += maxval-minval
+    sum_p2 += sum( (n1//n2 for (n1,n2) in it.permutations(numbers,r=2) if n1%n2==0) )
+
+inp.close()
+
+print(f'Part 1: {sum_p1}')
+print(f'Part 2: {sum_p2}')
+
