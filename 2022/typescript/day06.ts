587774fe58ae29789f1ab937841e1b77ba672637
André Santos <andrerfcsantos@gmail.com>
2022-12-09 20:31:21 +0000

    2022 - Day 06 - Typescript solution

---
+++ 2022/typescript/day06.ts
@@ -0,0 +1,32 @@
+import { runProblem, nonEmptyLines } from "./aoclib.ts";
+
+const DAY = 6;
+const YEAR = 2022;
+
+interface State {
+  signal: string;
+}
+
+export function parse(input: string): State {
+  return { signal: nonEmptyLines(input)[0] };
+}
+
+export function part1(parsed: State): string {
+  return seekMarker(parsed.signal, 4).toString();
+}
+
+export function part2(parsed: State): string {
+  return seekMarker(parsed.signal, 14).toString();
+}
+
+export function seekMarker(signal: string, markerSize: number): number {
+  for (let i = markerSize; i < signal.length; i += 1) {
+    const lastX = signal.slice(i - markerSize, i);
+    if (lastX.length === new Set(lastX).size) {
+      return i;
+    }
+  }
+  return NaN;
+}
+
+await runProblem<State>(DAY, YEAR, parse, part1, part2);
