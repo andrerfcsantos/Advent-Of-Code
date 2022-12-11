aae2e89e5a3f5cc3370cca3f403d2db12fcd5ece
André Santos <andrerfcsantos@gmail.com>
2022-12-11 16:59:24 +0000

    2022 - Refactor day 11 into taking a relax function parameter

--- 2022/typescript/day11.ts
+++ 2022/typescript/day11.ts
@@ -5,7 +5,7 @@ const YEAR = 2022;
 
 interface Monkey {
   number: number;
-  startingItems: number[];
+  items: number[];
   divisibleTtest: number;
   ifTrueThrow: number;
   ifFalseThrow: number;
@@ -16,7 +16,7 @@ interface State {
   monkeys: Monkey[];
 }
 
-type Op = (old: number) => number | undefined;
+type Op = (old: number) => number;
 
 const monkeyRegex =
   /Monkey (?<monkeyNo>\d+):\r?\n\s*Starting items: (?<numbers>.*)\r?\n\s*Operation: new = old (?<op>.) (?<arg2>.*)\r?\n\s*Test: divisible by (?<divisbleBy>\d+)\r?\n\s*If true: throw to monkey (?<throwTrue>\d+)\r?\n\s*If false: throw to monkey (?<throwFalse>\d+)/m;
@@ -56,7 +56,7 @@ export function parse(input: string): State {
 
   const monkeys = nonEmptyLines(input, newlines).map((monkey) => {
     const { groups } = monkeyRegex.exec(monkey)!;
-    const startingItems = groups!.numbers
+    const items = groups!.numbers
       .split(", ")
       .map((n: string) => parseInt(n, 10));
 
@@ -67,7 +67,7 @@ export function parse(input: string): State {
 
     return {
       number: parseInt(groups!.monkeyNo, 10),
-      startingItems,
+      items,
       divisibleTtest: parseInt(groups!.divisbleBy, 10),
       ifTrueThrow: parseInt(groups!.throwTrue, 10),
       ifFalseThrow: parseInt(groups!.throwFalse, 10),
@@ -83,27 +83,25 @@ export function parse(input: string): State {
 function computeMonkeyBusiness(
   monkeys: Monkey[],
   rounds: number,
-  relaxfactor?: number
+  relaxFunction: (worryLevel: number) => number
 ): number {
   const counters = new Array(monkeys.length).fill(0);
 
   for (let i = 0; i < rounds; i++) {
     for (let j = 0; j < monkeys.length; j++) {
       const monkey = monkeys[j];
-      counters[j] += monkey.startingItems.length;
-      while (monkey.startingItems.length != 0) {
-        const item = monkey.startingItems.shift();
+      counters[j] += monkey.items.length;
+      while (monkey.items.length != 0) {
+        const item = monkey.items.shift();
         const newWorryLevel = monkey.operation(item!)!;
-        const relaxedWorryLevel = relaxfactor
-          ? newWorryLevel % relaxfactor
-          : Math.floor(newWorryLevel / 3);
+        const relaxedWorryLevel = relaxFunction(newWorryLevel);
 
         const monkeyToThrow =
           relaxedWorryLevel % monkey.divisibleTtest == 0
             ? monkey.ifTrueThrow
             : monkey.ifFalseThrow;
 
-        monkeys[monkeyToThrow].startingItems.push(relaxedWorryLevel);
+        monkeys[monkeyToThrow].items.push(relaxedWorryLevel);
       }
     }
   }
@@ -114,14 +112,17 @@ function computeMonkeyBusiness(
 }
 
 export function part1(parsed: State): string {
-  return computeMonkeyBusiness(parsed.monkeys, 20).toString();
+  return computeMonkeyBusiness(parsed.monkeys, 20, (worryLevel) =>
+    Math.floor(worryLevel / 3)
+  ).toString();
 }
 
 export function part2(parsed: State): string {
+  const relaxDivisor = lcm(...parsed.monkeys.map((m) => m.divisibleTtest));
   return computeMonkeyBusiness(
     parsed.monkeys,
     10_000,
-    lcm(...parsed.monkeys.map((m) => m.divisibleTtest))
+    (worryLevel) => worryLevel % relaxDivisor
   ).toString();
 }
 
